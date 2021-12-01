93d54ebe188e06a6aeb378447a806c6969ee5a14
bevis_lin <bevis_lin@trendmicro.com>
2021-12-01 16:45:18 +0800

    final

--- budget.py
+++ budget.py
@@ -1,4 +1,7 @@
 import calendar
+from datetime import timedelta
+from typing import Collection
+from collections import Counter 
 class Budget:
     def __init__(self, yearmonth, amount) -> None:
         self.year_month = yearmonth
@@ -17,11 +20,27 @@ class BudgetService:
         b = self.get_all_budgets()
 
         year_month_of_start = start.strftime("%Y%m")
+
+        load_days = []
+        start_date = start
+        end_date = end
+        shift_time = timedelta(days=1)
+        while start_date <= end_date:
+            load_days.append(start_date.strftime("%Y%m"))
+            start_date = start_date + shift_time
+        
+        count = Counter(load_days)
+
+        # year_month_of_start = start.strftime("%Y%m")
         budget_of_start_month = 0
-        for budget in b:
-            if budget.year_month == year_month_of_start:
-                budget_of_start_month = budget.amount
-        else:
-            budget_of_start_month = 0
-        start_month_day_num = calendar.monthrange(start.year, start.month)[1]
-        return budget_of_start_month / start_month_day_num
+        total = 0
+        for year_month in count:
+            for budget in b:
+                if budget.year_month == year_month:
+                    budget_of_month = budget.amount
+                    month_day_num = calendar.monthrange(int(year_month[:4]), int(year_month[4:]))[1]
+                    total += budget_of_month / month_day_num * count[year_month]
+        # start_month_day_num = calendar.monthrange(start.year, start.month)[1]
+        
+    
+        return total
--- test_budget.py
+++ test_budget.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import date
 
-from budget import BudgetService
+from budget import Budget, BudgetService
 
 
 
@@ -22,6 +22,37 @@ class BudgetServiceTestCase(unittest.TestCase):
         self.b.get_all_budgets = self.my_budgets
         result = self.b.query(date(2021, 3, 1), date(2021, 3, 1))
         self.assertEqual(0, result)
+    
+    def test_one_day_only(self):
+        self.fake_budgets = [Budget("202103",31)]
+        self.b.get_all_budgets = self.my_budgets
+        result = self.b.query(date(2021, 3, 1), date(2021, 3, 1))
+        self.assertEqual(1.0, result)
+    
+    def test_multiply_day_in_same_month(self):
+        self.fake_budgets = [Budget("202103",31), Budget("202104", 300)]
+        self.b.get_all_budgets = self.my_budgets
+        result = self.b.query(date(2021, 3, 1), date(2021, 3, 6))
+        self.assertEqual(6.0, result)
+    
+    def test_multiply_day_in_one_month(self):
+        self.fake_budgets = [Budget("202103",31), Budget("202104", 300)]
+        self.b.get_all_budgets = self.my_budgets
+        result = self.b.query(date(2021, 3, 1), date(2021, 3, 31))
+        self.assertEqual(31.0, result)
+    
+    def test_multiply_day_in_diff_month(self):
+        self.fake_budgets = [Budget("202103",31), Budget("202104", 300)]
+        self.b.get_all_budgets = self.my_budgets
+        result = self.b.query(date(2021, 3, 30), date(2021, 4, 3))
+        self.assertEqual(32.0, result)
+
+    def test_multiply_day_in_multiple_month(self):
+        self.fake_budgets = [Budget("202103",31), Budget("202104", 300), Budget("202106", 6000)]
+        self.b.get_all_budgets = self.my_budgets
+        result = self.b.query(date(2021, 3, 30), date(2021, 6, 7))
+        self.assertEqual(1702.0, result)
+
 
 if __name__ == "__main__":
     unittest.main()
