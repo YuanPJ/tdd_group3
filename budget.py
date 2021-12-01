0ff1c40b17c63eb6e40331636e8601ea18e9d3b0
PJ Yuan <pj_yuan@trendmicro.com>
2021-12-01 15:48:59 +0800

    fake data

--- budget.py
+++ budget.py
@@ -2,10 +2,14 @@ class Budget:
     def __init__(self, yearmonth, amount) -> None:
         self.year_month = yearmonth
         self.amount = amount
-    
+
 class BudgetService:
     def __init__(self):
         pass
 
-    def query(self, start, end):
+    def get_all_budgets(self):
         pass
+
+    def query(self, start, end):
+        b = self.get_all_budgets()
+        return b[0].amount
--- test_budget.py
+++ test_budget.py
@@ -1,16 +1,21 @@
 import unittest
+from datetime import date
 
-from budget import BudgetService
+from .budget import BudgetService, Budget
+
 
 class BudgetServiceTestCase(unittest.TestCase):
     def setUp(self):
-        b = BudgetSevice()
+        self.b = BudgetService()
 
-    def my_budgets(budgets):
-        return
+    def my_budgets(self):
+        return self.fake_budgets
 
     def test_what(self):
-        b.get_all_budgets =
+        self.b.get_all_budgets = self.my_budgets
+        self.fake_budgets = [Budget("202103", 31)]
+        a = self.b.query(date(2021, 3, 1), date(2021, 3, 1))
+        assert a == 31
         pass
 
 if __name__ == "__main__":
