a99b2bac0022567de8600612400fbc3777f8df7d
PJ Yuan <pj_yuan@trendmicro.com>
2021-12-01 15:28:56 +0800

    add test

---
+++ test_budget.py
@@ -0,0 +1,17 @@
+import unittest
+
+from budget import BudgetService
+
+class BudgetServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        b = BudgetSevice()
+
+    def my_budgets(budgets):
+        return
+
+    def test_what(self):
+        b.get_all_budgets =
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
