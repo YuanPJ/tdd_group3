96d8cc29fbf8e86a5d2097144a0de9917d0c44a3
bevis_lin <bevis_lin@trendmicro.com>
2021-12-01 16:25:37 +0800

    test

--- budget.py
+++ budget.py
@@ -1,3 +1,4 @@
+import calendar
 class Budget:
     def __init__(self, yearmonth, amount) -> None:
         self.year_month = yearmonth
@@ -15,3 +16,12 @@ class BudgetService:
             return 0
         b = self.get_all_budgets()
 
+        year_month_of_start = start.strftime("%Y%m")
+        budget_of_start_month = 0
+        for budget in b:
+            if budget.year_month == year_month_of_start:
+                budget_of_start_month = budget.amount
+        else:
+            budget_of_start_month = 0
+        start_month_day_num = calendar.monthrange(start.year, start.month)[1]
+        return budget_of_start_month / start_month_day_num
--- test_budget.py
+++ test_budget.py
@@ -16,5 +16,12 @@ class BudgetServiceTestCase(unittest.TestCase):
         result = self.b.query(date(2021, 3, 3), date(2021, 3, 1))
         self.assertEqual(0, result)
 
+
+    def test_db_no_data(self):
+        self.fake_budgets = []
+        self.b.get_all_budgets = self.my_budgets
+        result = self.b.query(date(2021, 3, 1), date(2021, 3, 1))
+        self.assertEqual(0, result)
+
 if __name__ == "__main__":
     unittest.main()
